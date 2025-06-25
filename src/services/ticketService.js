5f933c04006c04cad652b432e36c9ada4e7539a3
“Kevincode6” <maloukevin6@gmail.com>
2025-06-25 07:29:16 +0200

    i added ticket

---
+++ src/services/ticketService.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+const API_URL = '/api/v1/tickets/';
+
+
+// create a new ticket 
+
+const createTicket = async (data, token) => {
+    const res = await axios.post(API_URL, data, {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+    return res.data.ticket;
+};
+
+// get all tickets for logged-in user
+
+const getMyTickets = async (token) => {
+    const res = await axios.get(API_URL + 'my-tickets', {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+    return res.data.data;
+};
+
+// get a ticket by ID
+
+const getTicket = async (id, token) => {
+    const res = await axios.get(API_URL + id, {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+    return res.data.data;
+};
+
+// updaate ticket 
+
+const updateTicket =  async (id, data, token) => {
+    const res = await axios.get(API_URL + id, data, {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+    return res.data.data;
+}
+
+export default { createTicket, getMyTickets, getTicket, updateTicket };
